a60c5d2925db831b41746a5aa4b2cf7e0bc70a24
failwin@ukr.net <Compaq6720s1>
2019-01-07 18:28:56 +0200

    added server couner

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+var count = 1;
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! ' + count);
+  count++;
 });
 
 app.use('/static', express.static('./static'));
